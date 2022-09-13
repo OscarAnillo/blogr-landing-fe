ba76c89046efcf62a3f2680ca2bcf6c021e03782
oscar anillo <oscarani14@gmail.com>
2022-09-13 14:08:56 -0500

    add navComponent without styles

--- src/components/nav-component.js
+++ src/components/nav-component.js
@@ -1,14 +1,54 @@
+import { useState } from 'react';
+
 export default function NavComponent(){
+    const [showMenu, setShowMenu] = useState(false);
+
+    const clickHandler = () => {
+        setShowMenu(!showMenu);
+    }
+
     return (
-        <section>
-            <nav>
+        <nav className="nav">
+            <section>
                 <div>
                     <img src="/images/logo.svg" alt="" />
                 </div>
                 <div>
-                    <img src="/images/icon-hamburger.svg" alt="" />
+                    {!showMenu ?
+                    <img src="/images/icon-hamburger.svg" alt="" onClick={clickHandler} /> : <img src="/images/icon-close.svg" alt="" onClick={clickHandler} /> 
+                    }
                 </div>
-            </nav>
-        </section>
+            </section>
+            {showMenu ? 
+            <div>
+                <div>
+                    <ul>
+                        <li>Product
+                            <img src="/images/icon-arrow-dark.svg" alt="" />
+                        </li>
+                        <li>Company
+                            <img src="/images/icon-arrow-dark.svg" alt="" />
+                        </li>
+                        <ul>
+                            <li>Connect
+                                <img src="/images/icon-arrow-dark.svg" alt="" />
+                            </li>
+                            <div>
+                                <p>Contact</p>
+                                <p>Newsletter</p>
+                                <p>LinkedIn</p>
+                            </div>
+                        </ul>
+                    </ul>
+                </div>
+            <div>
+                <p>Login</p>
+                <button>Sign Up</button>
+            </div>
+            </div>
+            : ''}
+        </nav>
     )
 }
+
+
