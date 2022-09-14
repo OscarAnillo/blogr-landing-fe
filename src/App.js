b80b1e939df687ccab735a34d1be2518f4d08700
oscar anillo <oscarani14@gmail.com>
2022-09-13 21:26:14 -0500

    add hero component and its styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import HeroComponent from './components/hero-component';
 import NavComponent from './components/nav-component';
 
 function App() {
   return (
     <div className="App">
       <NavComponent />
+      <HeroComponent />
     </div>
   );
 }
---
+++ src/components/hero-component.js
@@ -0,0 +1,14 @@
+export default function HeroComponent(){
+    return (
+        <div className="hero">
+            <div className="hero-bg"></div> 
+            <div className="hero-text">
+                <h1>A modern publishing platform</h1>
+                <p>Grow your audience and build your online brand</p>
+                <button>Start for Free</button>
+                <button>Learn More</button>
+            </div>
+        </div>
+    )
+}
+
