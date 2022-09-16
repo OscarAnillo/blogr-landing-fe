8b004a9327d32d4a688c961994b2eb4c287ca17f
oscar anillo <oscarani14@gmail.com>
2022-09-16 12:30:48 -0500

    update frontend mentor html elements errors

--- src/components/nav-component.js
+++ src/components/nav-component.js
@@ -1,97 +1,135 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-export default function NavComponent(){
-    const [showMenu, setShowMenu] = useState(false);
-    const [showConnect, setShowConnect] = useState(false)
+export default function NavComponent() {
+  const [showMenu, setShowMenu] = useState(false);
+  const [showConnect, setShowConnect] = useState(false);
 
-    const clickHandler = () => {
-        setShowMenu(!showMenu);
-    }
+  const clickHandler = () => {
+    setShowMenu(!showMenu);
+  };
 
-    const clickHandlerConnect = () => {
-        setShowConnect(!showConnect);
-    }
+  const clickHandlerConnect = () => {
+    setShowConnect(!showConnect);
+  };
 
-    return (
-        <nav className="nav">
-            <section>
-                <div className='div-desktop'>
-                    <div>
-                        <img src="/images/logo.svg" alt="" />
+  return (
+    <nav className="nav">
+      <section>
+        <div className="div-desktop">
+          <div>
+            <img src="/images/logo.svg" alt="" />
+          </div>
+          <div className="nav-desktop">
+            <ul>
+              <li>
+                Product
+                <img src="/images/icon-arrow-light.svg" alt="" />
+              </li>
+              <li>
+                Company
+                <img src="/images/icon-arrow-light.svg" alt="" />
+              </li>
+              <li>
+                Connect
+                {showConnect ? (
+                  <img
+                    src="/images/icon-arrow-light.svg"
+                    alt=""
+                    onClick={clickHandlerConnect}
+                    className="light"
+                  />
+                ) : (
+                  <img
+                    src="/images/icon-arrow-light.svg"
+                    alt=""
+                    onClick={clickHandlerConnect}
+                  />
+                )}
+                <ul>
+                  {showConnect ? (
+                    <div className="inner-menu-connect">
+                      <p>Contact</p>
+                      <p>Newsletter</p>
+                      <p>LinkedIn</p>
                     </div>
-                    <div className="nav-desktop">
-                        <ul>
-                            <li>Product
-                                <img src="/images/icon-arrow-light.svg" alt="" />
-                            </li>
-                            <li>Company
-                                <img src="/images/icon-arrow-light.svg" alt="" />
-                            </li>
-                            <ul>
-                                <li>Connect
-                                    {showConnect ? <img src="/images/icon-arrow-light.svg" alt="" onClick={clickHandlerConnect} className="light"/> :
-                                    <img src="/images/icon-arrow-light.svg" alt="" onClick={clickHandlerConnect} />
-                                    }
-                                </li>
-                                {showConnect ? 
-                                <div className='inner-menu-connect'>
-                                    <p>Contact</p>
-                                    <p>Newsletter</p>
-                                    <p>LinkedIn</p>
-                                </div>
-                                : ''}
-                            </ul>
-                        </ul>
+                  ) : (
+                    ""
+                  )}
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="div-desktop-login">
+          <div>
+            <p>Login</p>
+          </div>
+          <div>
+            <button>Sign Up</button>
+          </div>
+        </div>
+        <div className="nav-mobile">
+          {!showMenu ? (
+            <img
+              src="/images/icon-hamburger.svg"
+              alt=""
+              onClick={clickHandler}
+            />
+          ) : (
+            <img src="/images/icon-close.svg" alt="" onClick={clickHandler} />
+          )}
+        </div>
+      </section>
+      {showMenu ? (
+        <div className="inner-menu">
+          <div>
+            <ul>
+              <li>
+                Product
+                <img src="/images/icon-arrow-dark.svg" alt="" />
+              </li>
+              <li>
+                Company
+                <img src="/images/icon-arrow-dark.svg" alt="" />
+              </li>
+              <li style={{ color: "rgb(121, 133, 145)" }}>
+                Connect
+                {showConnect ? (
+                  <img
+                    src="/images/icon-arrow-dark.svg"
+                    alt=""
+                    onClick={clickHandlerConnect}
+                    className="light"
+                  />
+                ) : (
+                  <img
+                    src="/images/icon-arrow-dark.svg"
+                    alt=""
+                    onClick={clickHandlerConnect}
+                  />
+                )}
+                <ul>
+                  {showConnect ? (
+                    <div className="inner-menu-connect">
+                      <p>Contact</p>
+                      <p>Newsletter</p>
+                      <p>LinkedIn</p>
                     </div>
-                </div>
-                <div className="div-desktop-login">
-                    <div>
-                        <li>Login</li>
-                    </div>
-                    <div>
-                        <button>Sign Up</button>
-                    </div>
-                </div>
-                <div className="nav-mobile">
-                    {!showMenu ?
-                    <img src="/images/icon-hamburger.svg" alt="" onClick={clickHandler} /> : <img src="/images/icon-close.svg" alt="" onClick={clickHandler} /> 
-                    }
-                </div>
-            </section>
-            {showMenu ? 
-            <div className='inner-menu'>
-                <div>
-                    <ul>
-                        <li>Product
-                            <img src="/images/icon-arrow-dark.svg" alt="" />
-                        </li>
-                        <li>Company
-                            <img src="/images/icon-arrow-dark.svg" alt="" />
-                        </li>
-                        <ul>
-                            <li style={{color:"rgb(121, 133, 145)"}}>Connect
-                                {showConnect ? <img src="/images/icon-arrow-dark.svg" alt="" onClick={clickHandlerConnect} className="light"/> :
-                                <img src="/images/icon-arrow-dark.svg" alt="" onClick={clickHandlerConnect} />
-                                }
-                            </li>
-                            {showConnect ? 
-                            <div className='inner-menu-connect'>
-                                <p>Contact</p>
-                                <p>Newsletter</p>
-                                <p>LinkedIn</p>
-                            </div>
-                            : ''}
-                        </ul>
-                    </ul>
-                </div>
-            <div className='login'>
-                <p>Login</p>
-                <button>Sign Up</button>
-            </div>
-            </div>
-            : ''}
-        </nav>
-    )
+                  ) : (
+                    ""
+                  )}
+                </ul>
+              </li>
+            </ul>
+          </div>
+          <div className="login">
+            <p>Login</p>
+            <button>Sign Up</button>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+    </nav>
+  );
 }
-
-
